f82ad785d518e28773e42bcaa80f50167bb987ed
hibiki31 <corynarigato@gmail.com>
2023-02-23 04:34:11 +0000

    Change Dev Port

--- api/main.py
+++ api/main.py
@@ -92,4 +92,4 @@ def read_api_users_me():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True, access_log=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, access_log=False)
